2419c09c70e9dd42f9b28fea76be80a83bbe2e6c
Ben Christensen <bmchrist@mtu.edu>
2014-01-05 14:30:18 -0500

    Basic functionality

---
+++ timer.js
@@ -0,0 +1,50 @@
+var previous_total = 0;
+var time_opened;
+var open = 0;
+
+chrome.tabs.onUpdated.addListener( function callback( tabId, changeInfo, tab) {
+  check_tabs();
+});
+chrome.tabs.onRemoved.addListener(function(){
+  check_tabs();
+});
+
+check_tabs();
+
+function check_tabs(){
+  query = new Object();
+  query.url = "*://*.facebook.com/*"
+  chrome.tabs.query(query, function(tabs){
+    if( tabs.length > 0){ // Facebook is open somewhere
+      if( open == 0){ // If we just opened it:
+        open = 1;
+        // Record this as the time it was opened
+        time_opened = new Date().getTime();
+        console.log("Facebook opened at " + time_opened);
+        
+      }
+    }
+    if( tabs.length == 0){ // Facebook is not open
+      if( open == 1){ // If we just closed it:
+        open = 0;
+        console.log("Facebook closed");
+        // Store the duration it was open in previous_total
+        previous_total += (new Date().getTime()) - time_opened;
+      }
+    }
+  })
+}
+
+function get_time(){
+  if( open == 1){
+    return previous_total + (new Date().getTime()) - time_opened;
+  }
+  else{
+    return previous_total;
+  }
+}
+
+function timer()
+{
+  window.seconds_wasted++;
+}
---
+++ updater.js
@@ -0,0 +1,24 @@
+var views = chrome.extension.getViews();
+for (var i = 0; i < views.length; i++) {
+  if (views[i].location.pathname == "/_generated_background_page.html"){
+    var view = views[i]
+  }
+}
+
+timer = document.getElementById("timer");
+
+// Update every second while it's open
+setInterval(update, 1000)
+update();
+
+function update(){
+  milli = view.get_time();
+
+  var seconds = Math.floor((milli / 1000) % 60);
+  var minutes = Math.floor((milli / (60 * 1000)) % 60);
+  var hours = Math.floor((milli / (3600 * 1000)) % 60);
+  
+  timer.innerHTML = hours  + ":" + minutes + ":" + seconds;
+}
+
+
