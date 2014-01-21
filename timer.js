05773cf2e61c53c188f5b8e5128114c9888db827
Phil Middleton <pjmiddle@mtu.edu>
2014-01-21 16:49:21 -0500

    added functionality for multiple sites

--- timer.js
+++ timer.js
@@ -1,6 +1,11 @@
-var previous_total = 0;
-var time_opened;
-var open = 0;
+var previous_total = [0,0];
+var time_opened = new Array(2);
+var open = [0,0];
+var urls = [
+  ["*://*.facebook.com/*", "Facebook"],
+  ["*://*.twitter.com/*", "Twitter"]
+]
+
 
 chrome.tabs.onUpdated.addListener( function callback( tabId, changeInfo, tab) {
   check_tabs();
@@ -26,50 +31,72 @@ chrome.idle.onStateChanged.addListener(function(status){
 check_tabs();
 
 function check_tabs(){
-  query = new Object();
+  for(index = 0; index < urls.length; index++){
+    tabs_aysnch_call(index, urls[index][0]);
+  }
+}
+
+function get_urls(){
+  return urls
+}
+
+function tabs_aysnch_call(index, url){
+  query = new Object()
   query.active = true
-  query.url = "*://*.facebook.com/*"
+  query.url = url;
   chrome.tabs.query(query, function(tabs){
-    if( tabs.length > 0){ // Facebook is open somewhere
-      facebook_opened();
+    if( tabs.length > 0){ // Facebook is open somewhere)
+      facebook_opened(index);
     }
     if( tabs.length == 0){ // Facebook is not open
-      facebook_closed();
+      facebook_closed(index);
     }
   })
 }
 
-function facebook_opened(){
-  if( open == 0){ // If we just opened it:
-    open = 1;
+function facebook_opened(index){
+  if(open[index] == 0){ // If we just opened it:
+    open[index] = 1;
+    console.log(urls[index][0] + " has activated")
     // Record this as the time it was opened
-    time_opened = new Date().getTime();
-    console.log("Facebook active at " + time_opened);
+    time_opened[index] = new Date().getTime();
+    console.log("Sites active at " + time_opened[index]);
   }
 }
-function facebook_closed(){
-  if( open == 1){ // If we just closed it:
-    open = 0;
-    console.log("Facebook deactivated");
+function facebook_closed(index){
+  if( open[index] == 1){ // If we just closed it:
+    open[index] = 0;
+    console.log(urls[index][0] + " has deactivated");
     // Store the duration it was open in previous_total
-    previous_total += (new Date().getTime()) - time_opened;
+    previous_total[index] += (new Date().getTime()) - time_opened[index];
+    console.log("All sites deactivated " + time_opened[index])
   }
 }
 
-function get_time(){
-  if( open == 1){
-    return previous_total + (new Date().getTime()) - time_opened;
+function get_time(index){
+  if( open[index] == 1){
+    return previous_total[index] + (new Date().getTime()) - time_opened[index];
   }
   else{
-    return previous_total;
+    return previous_total[index];
   }
 }
 
-setInterval(calculate_badge, 30000);
+setInterval(calculate_badge, 15000);
 calculate_badge();
 
 function calculate_badge(){
-   update_badge( Math.floor(get_time() / (60*1000)) );
+  total = 0
+  for(index = 0;  index < urls.length; index++){
+    // console.log(urls[index][0], index, get_time(index))
+    total += get_time(index);
+  }    
+
+  // For each url
+    // total + =get time ( their index )
+  // update_badge(total)
+
+   update_badge( Math.floor(get_time(0) / (60*1000)) );
 }
 
 function update_badge(minutes){
